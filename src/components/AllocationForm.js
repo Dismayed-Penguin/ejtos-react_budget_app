913e23a67fac7ca4a631218378bfac7724243430
pignans <pignans@live.co.uk>
2023-08-07 11:10:44 +0100

    aligned currency with input

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -60,13 +60,15 @@ const AllocationForm = (props) => {
                         <option value="Reduce" name="Reduce">Reduce</option>
                     </select>
 
-                    <div>
+
+                    <div style={{ marginLeft: '2rem' }}>
+                        <span>{currency}</span>
                         <input
                             required='required'
                             type='number'
                             id='cost'
                             value={cost}
-                            style={{ marginLeft: '2rem' , size: 10}}
+                            style={{size: 10}}
                             onChange={(event) => setCost(event.target.value)}>
                         </input>
                     </div>
